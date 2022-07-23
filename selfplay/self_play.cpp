a9c200b02444298a0b6dd8a433f0b96dd1af7bf1
toame <m21j4012c@edu.tottori-u.ac.jp>
2022-07-23 21:10:01 +0900

    更新

--- selfplay/self_play.cpp
+++ selfplay/self_play.cpp
@@ -955,7 +955,8 @@ UCTSearcher::InterruptionCheck(const int playout_count, const int extension_time
 	int max = 0, second = 0;
 	const int child_num = root_node->child_num;
 	int limit_playout = color == Black ? (int(playouts_level[pattern][pos_id] * 1.2 + 20)) : max_playout_num * 1.5;
-	if (extension_times == 0) limit_playout = std::max(300, limit_playout) + child_num;
+	if (extension_times == 0) limit_playout = std::max(300, limit_playout);
+	limit_playout += child_num;
 	const int rest = limit_playout - playout_count;
 	const child_node_t* uct_child = root_node->child.get();
 
